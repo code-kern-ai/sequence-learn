4efc03f4e275d066f4a056db8dc01308d0a5853b
Johannes Hötter <jhoetter@MacBook-Pro-von-Johannes.local>
2022-04-30 23:36:28 +0200

    add standard classifiers for point classification

--- sequencelearn/point_tagger.py
+++ sequencelearn/point_tagger.py
@@ -1,6 +1,42 @@
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.svm import SVC
 from sequencelearn.base import CONSTANT_OUTSIDE, PointTagger
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+
+
+class SupportVectorTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = SVC(**kwargs)
+
+
+class LogisticTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = LogisticRegression(**kwargs)
+
+
+class NearestNeighborTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = KNeighborsClassifier(**kwargs)
+
+
+class BayesTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = GaussianNB(**kwargs)
+
+
+class GaussianTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = GaussianProcessClassifier(**kwargs)
 
 
 class TreeTagger(PointTagger):
@@ -13,3 +49,9 @@ class ForestTagger(PointTagger):
     def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
         super().__init__(constant_outside)
         self.model = RandomForestClassifier(**kwargs)
+
+
+class AdaTagger(PointTagger):
+    def __init__(self, constant_outside=CONSTANT_OUTSIDE, **kwargs):
+        super().__init__(constant_outside)
+        self.model = AdaBoostClassifier(**kwargs)
